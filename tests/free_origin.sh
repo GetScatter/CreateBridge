9930ccc74cef63605ad59e28d687c2d75d7ccd36
Surabhi Lodha <surabhil@usc.edu>
2019-02-25 11:52:00 -0800

    modify test

--- tests/free_origin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NAME=${1:-scatter11tst}
-ORIGIN=${2:-everipedia.org}
-MEMO=${3:-asdf}
-
-#cleos transfer eosio createbridge "10.0000 SYS" "$MEMO" -p eosio@active
-#cleos transfer eosio createbridge "10.0000 SYS" "$ORIGIN::free" -p eosio@active
-cleos push action createbridge create '["'$MEMO'", "'$NAME'", "EOS7w5aJCv5B7y3a6f4WCwPSvs6TpCAoRGnGpiLMsSWbmxaZdKigd", "'$ORIGIN'"]' -p createbridge@active
